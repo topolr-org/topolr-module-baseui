06b142c8bc01f20d6e5e0c5028ea0d6261b9c802
hou80houzhu <hou80houzhu@vip.qq.com>
2017-06-15 18:48:21 +0800

    init

---
+++ com/topolr/baseui/popup.js
@@ -0,0 +1,34 @@
+/**
+ * @packet baseui.popup;
+ * @style baseui.style.msgbox;
+ * @template baseui.template.temp;
+ */
+Module({
+    name:"messagebox",
+    extend:"viewgroup",
+    className:"messagebox",
+    style:"@msgbox",
+    layout:"@temp.msgbox",
+    option:{
+        width:"500px",
+        title:"msgbox",
+        content:[],
+        btns:[{name:"",type:""}]
+    },
+    oninitchild:function (module) {
+        var id=module.getId();
+        if(this.option.content[id]) {
+            $.extend(module.option, this.option.content[id].option);
+        }
+    },
+    bind_btn:function (dom) {
+        if(dom.cache().type==="close"){
+            this.remove();
+        }else {
+            this.dispatchEvent("btn", dom.cache());
+        }
+    },
+    close:function () {
+        this.remove();
+    }
+});
--- com/topolr/baseui/test.js
+++ com/topolr/baseui/test.js
@@ -1,17 +1,44 @@
 /**
  * @packet baseui.test;
  * @require baseui.loading;
+ * @require baseui.popup;
  * @require icon.iconpreviewer;
  */
+Module({
+    name:"test",
+    extend:"view",
+    template:"<div>{{data.text}}</div>",
+    option:{
+        text:""
+    },
+    init:function () {
+        this.render(this.option);
+    }
+});
 Option({
     name:'boot',
     option:{
         override:{
             onendinit:function () {
+                // this.addChild({
+                //     type:"@loading.area"
+                // }).then(function (area) {
+                //     area.showError("loading...");
+                // });
                 this.addChild({
-                    type:"@loading.area"
-                }).then(function (area) {
-                    area.showError("loading...");
+                    type:"@popup.messagebox",
+                    option:{
+                        width:"300px",
+                        btns:[
+                            {name:"ok",type:"ok"},
+                            {name:"close",type:"close"}
+                        ],
+                        content:[
+                            {type:"@.test",option:{text:"aa"}},
+                            {type:"@.test",option:{text:"bb"}},
+                            {type:"@.test",option:{text:"cc"}}
+                        ]
+                    }
                 });
             }
         }
