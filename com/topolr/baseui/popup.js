df0bb2eee8dafff57bb6c50f84758918ad24e065
hou80houzhu <hou80houzhu@vip.qq.com>
2017-08-28 19:40:14 +0800

    init

--- com/topolr/baseui/popup.js
+++ com/topolr/baseui/popup.js
@@ -15,21 +15,21 @@ Module({
         content: [],
         btns: [{name: "", type: ""}]
     },
-    init:function () {
-        var info=this.getQueryInfo();
-        if(info.popup){
-            info.popup=info.popup+":"+this.instanceId();
-        }else{
-            info.popup=this.instanceId();
+    init: function () {
+        var info = this.getQueryInfo();
+        if (info.popup) {
+            info.popup = info.popup + ":" + this.instanceId();
+        } else {
+            info.popup = this.instanceId();
         }
-        var query=[];
-        for(var i in info){
-            query.push(i+"="+info[i]);
+        var query = [];
+        for (var i in info) {
+            query.push(i + "=" + info[i]);
         }
-        var url=window.location.href.substring(sitePath.length).split("?")[0]+"?"+query.join("&");
-        this._url=url;
-        this.dispatchEvent("openPage",{
-            url:url
+        var url = window.location.href.substring(sitePath.length).split("?")[0] + "?" + query.join("&");
+        this._url = url;
+        this.dispatchEvent("openPage", {
+            url: url
         });
     },
     oninitchild: function (module) {
@@ -45,34 +45,34 @@ Module({
             this.dispatchEvent("btn", dom.cache());
         }
     },
-    bind_back:function(){
+    bind_back: function () {
         this.close();
     },
     close: function () {
-        var info=this.getQueryInfo();
-        if(info.popup){
+        var info = this.getQueryInfo();
+        if (info.popup) {
             window.history.back();
-        }else{
+        } else {
             this.remove();
         }
     },
-    getQueryInfo:function () {
-        var baseurl=window.location.href.substring(sitePath.length);
-        var query=baseurl.split("?")[1];
-        if(query){
-            var _a=query.split("&");
-            var _b={};
-            for(var i=0;i<_a.length;i++){
-                var _c=_a[i].split("=");
-                _b[_c[0]]=_c[1];
+    getQueryInfo: function () {
+        var baseurl = window.location.href.substring(sitePath.length);
+        var query = baseurl.split("?")[1];
+        if (query) {
+            var _a = query.split("&");
+            var _b = {};
+            for (var i = 0; i < _a.length; i++) {
+                var _c = _a[i].split("=");
+                _b[_c[0]] = _c[1];
             }
             return _b;
-        }else{
+        } else {
             return {};
         }
     },
-    event_urlchange:function () {
-        if(window.location.href!=this._url) {
+    event_urlchange: function (e) {
+        if (window.location.href != this._url) {
             var info = this.getQueryInfo();
             if (info.popup) {
                 var ids = info.popup.split(":");
@@ -85,3 +85,27 @@ Module({
         }
     }
 });
+
+module.exports = {
+    resetPopups: function () {
+        var query = window.location.href.split("?")[1];
+        if (query) {
+            var _a = query.split("&"),_b={};
+            for (var i = 0; i < _a.length; i++) {
+                var _c = _a[i].split("=");
+                _b[_c[0]] = _c[1];
+            }
+            var querystr=[];
+            for(var i in _b){
+                if(i!=="popup") {
+                    querystr.push(i + "=" + _b[i]);
+                }
+            }
+            if(querystr.length>0) {
+                window.history.replaceState({}, "", window.location.href.split("?")[0] + "?" + querystr.join("&"));
+            }else{
+                window.history.replaceState({}, "", window.location.href.split("?")[0]);
+            }
+        }
+    }
+};
